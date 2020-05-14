16aca30b790be474a3d2df5e4f9f8c54ebba9fb9
Noe Hernandez <Nhernandez2020@hotmail.com>
2020-05-14 11:26:16 -0700

    added files to project

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const key = require("../key");
+const mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: key.key.secret,
+    database: "burgers_db",
+  });
+}
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting:", err.stack);
+    return;
+  }
+  console.log("connected as id", connection.threadId);
+});
+
+module.exports = connection;

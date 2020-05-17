887608a79ac87f64015978b6868edc49fe050e10
Noe Hernandez <Nhernandez2020@hotmail.com>
2020-05-16 17:33:28 -0700

    made changes to server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burger_controller");
 
 app.get("/", (req, res) => {
-  res.render("/api/burgers");
+  res.render("/assets/burgers.js");
 })
 app.use(routes);
 
